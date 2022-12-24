0a6973b3b412dd2bcd3bdafeda27dc155ec7eee2
saad khan <saadkhan108b@gmail.com>
2022-12-24 19:47:23 +0530

    completed

--- ticTacToe.py
+++ ticTacToe.py
@@ -36,14 +36,28 @@ while True:
         except KeyError or ValueError:
             print('invalid move')
     
-    if  (theBoard[1] == theBoard[4] == theBoard[7] and theBoard[1] != ' ' and theBoard[4] != ' ' and theBoard[7] != ' ' or 
-        theBoard[2] == theBoard[5] == theBoard[8] and theBoard[2] != ' ' and theBoard[5] != ' ' and theBoard[8] != ' ' or
-        theBoard[3] == theBoard[6] == theBoard[9] and theBoard[3] != ' ' and theBoard[6] != ' ' and theBoard[9] != ' ' or
-        theBoard[1] == theBoard[2] == theBoard[3] and theBoard[1] != ' ' and theBoard[2] != ' ' and theBoard[3] != ' ' or
-        theBoard[4] == theBoard[5] == theBoard[6] and theBoard[4] != ' ' and theBoard[5] != ' ' and theBoard[6] != ' ' or
-        theBoard[7] == theBoard[8] == theBoard[9] and theBoard[7] != ' ' and theBoard[8] != ' ' and theBoard[9] != ' ' or
-        theBoard[1] == theBoard[5] == theBoard[9] and theBoard[1] != ' ' and theBoard[5] != ' ' and theBoard[9] != ' ' or
-        theBoard[3] == theBoard[5] == theBoard[7] and theBoard[3] != ' ' and theBoard[5] != ' ' and theBoard[7] != ' '):
+    
         
-        print('some one won')
-        break
+    if (theBoard[1] == theBoard[4] == theBoard[7] and theBoard[1] == 'X' and theBoard[4] == 'X' and theBoard[7] == 'X' or 
+            theBoard[2] == theBoard[5] == theBoard[8] and theBoard[2] == 'X' and theBoard[5] == 'X' and theBoard[8] == 'X' or
+            theBoard[3] == theBoard[6] == theBoard[9] and theBoard[3] == 'X' and theBoard[6] == 'X' and theBoard[9] == 'X' or
+            theBoard[1] == theBoard[2] == theBoard[3] and theBoard[1] == 'X' and theBoard[2] == 'X' and theBoard[3] == 'X' or
+            theBoard[4] == theBoard[5] == theBoard[6] and theBoard[4] == 'X' and theBoard[5] == 'X' and theBoard[6] == 'X' or
+            theBoard[7] == theBoard[8] == theBoard[9] and theBoard[7] == 'X' and theBoard[8] == 'X' and theBoard[9] == 'X' or
+            theBoard[1] == theBoard[5] == theBoard[9] and theBoard[1] == 'X' and theBoard[5] == 'X' and theBoard[9] == 'X' or
+            theBoard[3] == theBoard[5] == theBoard[7] and theBoard[3] == 'X' and theBoard[5] == 'X' and theBoard[7] == 'X'):
+                
+                print("X won")
+                break
+            
+    elif (theBoard[1] == theBoard[4] == theBoard[7] and theBoard[1] == 'O' and theBoard[4] == 'O' and theBoard[7] == 'O' or 
+        theBoard[2] == theBoard[5] == theBoard[8] and theBoard[2] == 'O' and theBoard[5] == 'O' and theBoard[8] == 'O' or
+        theBoard[3] == theBoard[6] == theBoard[9] and theBoard[3] == 'O' and theBoard[6] == 'O' and theBoard[9] == 'O' or
+        theBoard[1] == theBoard[2] == theBoard[3] and theBoard[1] == 'O' and theBoard[2] == 'O' and theBoard[3] == 'O' or
+        theBoard[4] == theBoard[5] == theBoard[6] and theBoard[4] == 'O' and theBoard[5] == 'O' and theBoard[6] == 'O' or
+        theBoard[7] == theBoard[8] == theBoard[9] and theBoard[7] == 'O' and theBoard[8] == 'O' and theBoard[9] == 'O' or
+        theBoard[1] == theBoard[5] == theBoard[9] and theBoard[1] == 'O' and theBoard[5] == 'O' and theBoard[9] == 'O' or
+        theBoard[3] == theBoard[5] == theBoard[7] and theBoard[3] == 'O' and theBoard[5] == 'O' and theBoard[7] == 'O'):
+
+            print('O won')
+            break
